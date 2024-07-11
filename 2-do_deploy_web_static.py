33452afc1fe1c450ce1abd3be2ced423f99c5b14
MohamedAlaga <malaga.142014@gmail.com>
2024-07-12 01:25:57 +0300

    2-do_deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["35.175.132.56", "	54.157.130.43"]
 
 
 def do_deploy(archive_path):
