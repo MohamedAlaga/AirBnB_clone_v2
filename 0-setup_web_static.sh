62769483fdf2385627a1a95b15591a43e9805fb0
MohamedAlaga <malaga.142014@gmail.com>
2024-07-11 06:41:09 +0300

    0-setup_web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,6 @@
 apt-get -y update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-echo 'Hello World!' > /var/www/html/index.nginx-debian.html
-sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://github.com/MohamedAlaga permanent;' /etc/nginx/sites-available/default
-echo "Ceci n'est pas une page" > /usr/share/nginx/html/custom_404.html
-sed -i '/server_name _;/a error_page 404 /custom_404.html;\nlocation = /custom_404.html {\nroot /usr/share/nginx/html;\ninternal;\n}' /etc/nginx/sites-available/default
-sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
 service nginx start
 mkdir -p /data/
 mkdir -p /data/web_static/
@@ -17,5 +12,5 @@ mkdir -p /data/web_static/releases/test/
 echo "hello World! HBNB clone" >> /data/web_static/releases/test/index.html
 ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
-sed -i '/rewrite ^/redirect_me https://github.com/MohamedAlaga permanent;/a location /hbnb_static/{ alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sed -i '/rewrite ^/redirect_me https://github.com/MohamedAlaga permanent;/a \nlocation /hbnb_static/ { \nalias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
 service nginx restart
