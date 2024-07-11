65d85b3c9a833623fb7a22488eee90bf4c0c07d8
MohamedAlaga <malaga.142014@gmail.com>
2024-07-11 22:54:45 +0300

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-""" Fabric script (based on the file 2-do_deploy_web_static.py) that creates
-and distributes an archive to your web servers, using the function deploy: """
-
+"""
+fab file to deploy web static files to server
+"""
 
 from fabric.api import *
+from fabric.operations import run, put
 from datetime import datetime
-from os.path import exists
-# do_pack = __import__('1-pack_web_static').do_pack
-# do_deploy = __import__('2-do_deploy_web_static').do_deploy
+import os
 
-
-env.hosts = ['ubuntu@35.237.166.125', 'ubuntu@54.167.61.201']  # <IP web-01>, <IP web-02>
-# ^ All remote commands must be executed on your both web servers
-# (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
-
+env.hosts = ['ubuntu@35.175.132.56','ubuntu@54.157.130.43']
 
 def do_pack():
-    """generates a .tgz archive from the contents of the web_static folder
-    """
+    """ Fabric script that generates a .tgz archive from the contents of the...
+    ...web_static folder """
     local("sudo mkdir -p versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "versions/web_static_{}.tgz".format(date)
@@ -27,27 +22,26 @@ def do_pack():
     else:
         return None
 
-
 def do_deploy(archive_path):
     """ distributes an archive to my web servers
     """
-    if exists(archive_path) is False:
-         return False
+    if not os.path.exists(archive_path):
+        return False
     filename = archive_path.split('/')[-1]
     no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
     tmp = "/tmp/" + filename
-
     try:
         put(archive_path, "/tmp/")
         run("mkdir -p {}/".format(no_tgz))
         run("tar -xzf {} -C {}/".format(tmp, no_tgz))
         run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("mv -f {}/web_static/* {}/".format(no_tgz, no_tgz))
         run("rm -rf {}/web_static".format(no_tgz))
         run("rm -rf /data/web_static/current")
         run("ln -s {}/ /data/web_static/current".format(no_tgz))
         return True
-    except:
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return False
 
 def deploy():
@@ -55,7 +49,7 @@ def deploy():
     execute both do_pack and do_deploy
     """
     archive = do_pack()
-    if exists(archive) is False:
+    if os.path.exists(archive) is False:
         return False
     dep = do_deploy(archive)
     return dep
