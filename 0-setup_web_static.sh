6ea64c7b8c09025942400869b2795198ccc2f6a0
MohamedAlaga <malaga.142014@gmail.com>
2024-07-11 06:45:37 +0300

    0-setup_web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,5 +12,5 @@ mkdir -p /data/web_static/releases/test/
 echo "hello World! HBNB clone" >> /data/web_static/releases/test/index.html
 ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
-sed -i '/rewrite ^/redirect_me https://github.com/MohamedAlaga permanent;/a \nlocation /hbnb_static/ { \nalias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a \nlocation /hbnb_static/ { \nalias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
 service nginx restart
