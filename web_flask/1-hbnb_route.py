d297ba5bd593b0794dce045fc3ae1fbc245643e7
MohamedAlaga <malaga.142014@gmail.com>
2024-07-29 02:11:32 +0300

    hbnb_route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,5 +16,11 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ returns HBNB """
+    return 'c {}'.format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
