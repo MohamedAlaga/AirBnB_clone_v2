7b1aaf9e70957aa754bfd983f852e3dffdf17df1
MohamedAlaga <malaga.142014@gmail.com>
2024-07-29 02:18:23 +0300

    hbnb_route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,7 +22,7 @@ def c_text(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
-
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text = "is cool"):
     """ returns HBNB """
