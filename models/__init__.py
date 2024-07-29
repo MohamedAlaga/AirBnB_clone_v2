dc23931f58102899b93a66565482d272f059250e
MohamedAlaga <malaga.142014@gmail.com>
2024-07-29 04:06:20 +0300

    states_list

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""This module select the stroage engine"""
+"""
+initialize the models package
+"""
 
 from os import getenv
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine import db_storage
-    storage = db_storage.DBStorage()
-else:
-    from models.engine import file_storage
-    storage = file_storage.FileStorage()
 
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
