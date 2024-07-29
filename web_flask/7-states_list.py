23547ff4d198aa213f84fa903e4fcd4f0cd613e3
MohamedAlaga <malaga.142014@gmail.com>
2024-07-29 03:14:26 +0300

    engine improvments

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" module 7-states_list """
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def get_all_states():
+    """ returns a template with all states """
+    return render_template('7-states_list.html', states=storage.all(State).values())
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
