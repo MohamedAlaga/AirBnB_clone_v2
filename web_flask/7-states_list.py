ce7a8d2a71d4e0a244a043786745369b29053a7d
MohamedAlaga <malaga.142014@gmail.com>
2024-07-29 03:20:05 +0300

    engine improvments

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,6 +9,14 @@ app = Flask(__name__)
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def get_all_states():
     """ returns a template with all states """
-    return render_template('7-states_list.html', states=storage.all(State).values())
+    return render_template('7-states_list.html', states=storage.all("State").values())
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
